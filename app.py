b89872dc67b1eb32b4795c6b7e0aba3f4c40b7d3
Umar Farooq <141214440+umarfarooqdev123@users.noreply.github.com>
2026-02-18 07:22:50 +0500

    Setup the website with Flask bankend and also structure the folder according to industry standard

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
